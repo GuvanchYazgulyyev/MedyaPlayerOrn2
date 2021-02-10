c4bca6d05ca4ff049274c1a2570cef3f1f7e749a
Guvanch <mr.yazgulyyew@outlook.com>
2021-02-10 03:22:35 +0300

    C# MedyaPlayer Kullanımı

---
+++ Radio Csharp orn/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Radio_Csharp_orn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AxWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://www.canliradyodinle.fm//trt-nagme-radyo-dinle.html";
+        }
+    }
+}
